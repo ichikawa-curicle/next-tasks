22d660732d3641b05e53c947bc41e82e638a428b
ichikawa-curicle <ichikawa@curicle.jp>
2025-03-15 14:50:20 +0900

    メモ

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -11,10 +11,16 @@ export default async function SignInPage(props: { searchParams: Promise<{ callba
   }
   const searchParams = await props.searchParams
   const { callbackUrl = "/" } = searchParams
+
+  // TODO パスワードを忘れた場合はどうする？
+
   return (
     <div className="flex justify-center py-12">
       <div className="p-10 rounded-lg space-y-8 bg-gray-300">
         <SigninForm callbackUrl={callbackUrl} />
+        <div className="flex justify-center text-gray-500">
+          または
+        </div>
         {Object.values(providerMap).map((provider) => {
           return (
             <form
